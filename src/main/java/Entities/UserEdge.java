edb38d3d3ce780666aae55004b8f0398c8d8ed7b
Ewan Jordan <ewan.jordan30@gmail.com>
2022-11-02 19:59:44 -0400

    Added multiple basic methods of UserEdge and UserGraph. These include createEdge, deleteEdge(to-do), deleteUser(to-do), addUser, userInGraph (UserGraph) and hasMatch and likes, as well as like attributes, for UserEdge
    I used some dummy methods that will be implemented by User as well, so that where the errors are from.

--- src/main/java/Entities/UserEdge.java
+++ src/main/java/Entities/UserEdge.java
@@ -3,9 +3,25 @@ package Entities;
 public class UserEdge {
     private User[] users;
     private float weight;
-    public UserEdge(User user1, User user2){
+    private boolean likeForwards;
+    private boolean likeBackwards;
+    public UserEdge(User user1, User user2) {
         this.users = new User[2];
         this.users[0] = user1;
         this.users[1] = user2;
+        this.likeForwards = false;
+        this.likeBackwards = false;
+    }
+    public boolean hasMatch(){
+        // Returns whether a match exists between two users
+        return this.likeForwards || this.likeBackwards;
+    }
+    public void likes(User likee){
+        //In this case 'likes' is like a verb, the passed in user is 'liked' by the other user
+        if(this.users[0].getUsername == likee.getUsername){
+            //these are theoretical getter methods of User
+            this.likeBackwards = true;
+        }
+        else{ this.likeForwards = true;}
     }
 }
--- src/main/java/Entities/UserGraph.java
+++ src/main/java/Entities/UserGraph.java
@@ -18,4 +18,37 @@ private ArrayList<UserEdge> userEdges;
         this.users = users;
         this.userEdges = userEdges;
     }
+    public boolean userInGraph(User userCheck) {
+        //checks if username
+        for (User user : this.users) {
+            if(user.getUsername == userCheck.getUsername){
+                //theoretical getter methods of User
+                return true;
+            }
+        }
+        return false;
+    }
+    public void addUser(User user){
+        //Preconditions: user not in users
+        this.users.add(user);
+        for (User otherUser : this.users) {
+           this.createEdge(user, otherUser);
+           user.addNeighbor(otherUser);
+           otherUser.addNeighbour(user);
+           //more theoretical User methods
+    }
+    }
+    public void deleteUser(User user){
+        //TO DO
+    }
+
+    public void createEdge(User user1, User user2){
+        //Precondition: No edge exists between user1 and user2
+        //              user1 in users, user2 in users
+        UserEdge newEdge = new UserEdge(user1, user2);
+        this.userEdges.add(newEdge);
+    }
+    public void deleteEdge(User user){
+        //TO DO
+    }
 }
