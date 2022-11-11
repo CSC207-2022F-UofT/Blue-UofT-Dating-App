4c2224c888b2534f2699cff884ef73715b9530f6
clarkzhang <czhang0312>
2022-11-11 11:48:52 -0500

    Added View Interface and minor additions to account and other presenter

---
+++ src/main/java/InterfaceAdapters/AccountPresenter.java
@@ -0,0 +1,17 @@
+package InterfaceAdapters;
+
+public class AccountPresenter implements View {
+    // View interface
+    public void back(){
+        //calls method to change
+    }
+    public void edit(){
+        // create use case object
+        // call use case method from the use case object
+    }
+
+    public void deleteAccount() {
+        // calls use case to delete account
+    }
+
+}
--- src/main/java/UseCases/UserRegUseCase.java
+++ src/main/java/UseCases/UserRegUseCase.java
@@ -1,4 +1,5 @@
 package UseCases;
 
 public class UserRegUseCase {
+
 }
