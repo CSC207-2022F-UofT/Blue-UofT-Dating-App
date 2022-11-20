423b86a32c13ec363c28e1f593bd419b981ec0a0
kcccr123 <109507575+kcccr123@users.noreply.github.com>
2022-11-20 17:39:30 -0500

    FIXED VIEW SO IT FOLLOWS CLEAN ARCH

--- src/main/java/UseCases/OtherAccountCases.java
+++ src/main/java/UseCases/OtherAccountCases.java
@@ -1,22 +1,16 @@
 package UseCases;
 import Entities.User;
+import Entities.UserDataClasses.UserData;
 import Entities.UserGraph;
 
 public class OtherAccountCases {
     public String[] getUserInfo(String userid) {
-        // Questions:
-        // How do I access userGraph?
-        // Do I create a new one each time and add all the data in?
-        // is it created on program start and stored somewhere I can access?
-
-        //currently making new userGraph just for psuedo code purposes
         UserGraphReadWriter reader = new UserGraphReadWriter();
         UserGraph graph = reader.readFromFile("dog.png");
-        User userclass = graph.getUser(userid);
+        User userclass = graph.getUserByString(userid);
         String[] userInfo = {userclass.getDisplayName().getData(),
                 userclass.getLocation().getData(),
                 userclass.getBio().getData()};
-
         return userInfo;
     }
 }
