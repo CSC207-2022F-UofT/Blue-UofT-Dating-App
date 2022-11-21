d9757a029ebe14033c73ed9216fc666e58f009e1
Rudy <rudraksh.monga@mail.utoronto.ca>
2022-11-21 16:54:00 -0500

    getallchatrooms added

--- src/main/java/UseCases/ChatUseCases/ChatRepoUseCase.java
+++ src/main/java/UseCases/ChatUseCases/ChatRepoUseCase.java
@@ -1,16 +1,16 @@
 package UseCases.ChatUseCases;
 
 import Entities.Chatroom;
-import Entities.ChatroomFactory;
 import Entities.User;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class ChatRepoUseCase {
     //class for checking if a chatroom exists
 
-    private static final HashMap<Set<User>, Chatroom> currChatrooms = new HashMap<>();
+    private static final Map<Set<User>, Chatroom> currChatrooms = new HashMap<>();
 
     //will get called from chat reg, assume that the chatroom is legit
     public void addChatroom(Chatroom chat) {
@@ -23,6 +23,10 @@ public class ChatRepoUseCase {
 
     public Chatroom getChatroom(Set<User> users) {
         return currChatrooms.get(users);
+    }
+
+    public Map<Set<User>, Chatroom> getAllChatrooms() {
+        return currChatrooms;
     }
 
 }
