159808e05b7d9f2a3652919089e31b23c7a6d18e
kcccr123 <109507575+kcccr123@users.noreply.github.com>
2022-11-30 16:53:31 -0500

    otherAccount udpates

--- src/main/java/FrameworksDrivers/ViewUI.java
+++ src/main/java/FrameworksDrivers/ViewUI.java
@@ -1,11 +1,6 @@
 package FrameworksDrivers;
 
-import Entities.Chatroom;
-import Entities.Message;
 import Entities.User;
-import InterfaceAdapters.ChatViewPresenter;
-import UseCases.ChatUseCases.ChatRenderUseCase;
-import UseCases.ChatUseCases.ChatRepoUseCase;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,7 +12,6 @@ public class ViewUI {
     //master panel
     private JPanel masterPanel = new JPanel();
     private CardLayout layout = new CardLayout();
-    private View currView;
     public ViewUI() {
         //master panel
         this.masterPanel.setLayout(layout);
@@ -31,7 +25,6 @@ public class ViewUI {
         ChatView chatView = new ChatView(this.masterPanel, this.layout);
         SignUpView signUpView = new SignUpView(this.masterPanel);
 
-        currView = chatView;
 
 
         // Main Page
@@ -44,10 +37,7 @@ public class ViewUI {
         userEditView.sendPaths(userEditPaths);
         Object[] LogInPath = {accountView,signUpView};
         logInView.sendPaths(LogInPath);
-//        Object[] chatViewPaths = {MainPageView};
-
-        JScrollPane scroller = new JScrollPane( this.masterPanel);
-        this.layout.show(this.masterPanel, "chatView");
+        JScrollPane scroller = new JScrollPane( this.masterPanel );
         this.frame.add(scroller);
         this.frame.setDefaultCloseOperation(this.frame.EXIT_ON_CLOSE);
         this.frame.setTitle("MainPage");
@@ -59,24 +49,8 @@ public class ViewUI {
     }
 
     public static void main(String[] args) {
-        User u1 = new User("clark", "12345");
-        User u2 = new User("kevin", "54321");
-        Chatroom test = new Chatroom(u1, u2);
-        test.addMessage(new Message(u1, "hello kevin"));
-        test.addMessage(new Message(u2, "hello clark"));
-
-        u1 = new User("bob", "12345");
-        u2 = new User("joe", "54321");
-        Chatroom test2 = new Chatroom(u1, u2);
-        test2.addMessage(new Message(u1, "hello joe"));
-        test2.addMessage(new Message(u2, "hello bob"));
-
         ViewUI UI = new ViewUI();
-        ChatRepoUseCase repo = new ChatRepoUseCase();
-        repo.addChatroom(test);
-        repo.addChatroom(test2);
-
-        ChatViewPresenter presenter = new ChatViewPresenter(UI.currView);
-        presenter.render();
     }
+
+
 }
