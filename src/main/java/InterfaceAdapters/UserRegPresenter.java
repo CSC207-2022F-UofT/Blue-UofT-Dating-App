01e31be3c2573a58cfbf1f3649e91984bca4b27e
clarkzhang <czhang0312>
2022-11-30 17:57:57 -0500

    Added message functionality, sendMessage presenter method

--- src/main/java/InterfaceAdapters/UserRegPresenter.java
+++ src/main/java/InterfaceAdapters/UserRegPresenter.java
@@ -1,40 +1,40 @@
-package InterfaceAdapters;
-import FrameworksDrivers.LogInView;
-import FrameworksDrivers.View;
-
-
-public class UserRegPresenter{
-    // create fail and success views
-
-
-    public void updatePage(String view, Object page) {
-        switch(view) {
-            case "Username already exists.":
-                LogInView failView = (LogInView) page;
-
-                // call Interactor & Presenter
-                // sending back to ui
-
-                // don't have a the view message yet
-                failView.updateFailView("");
-                break;
-        }
-    }
-
-
-    public void updatePage(String logInView, LogInView page) {
-        switch(logInView) {
-            case "Passwords don't match.":
-                LogInView failView = (LogInView) page;
-
-                // call Interactor & Presenter
-                // sending back to ui
-
-                // don't have a  view message yet
-                failView.updateFailView("");
-                break;
-        }
-    }
-
-
-}
+//package InterfaceAdapters;
+////import FrameworksDrivers.LogInView;
+//import FrameworksDrivers.View;
+//
+//
+//public class UserRegPresenter{
+//    // create fail and success views
+//
+//
+//    public void updatePage(String view, Object page) {
+//        switch(view) {
+//            case "Username already exists.":
+//                LogInView failView = (LogInView) page;
+//
+//                // call Interactor & Presenter
+//                // sending back to ui
+//
+//                // don't have a the view message yet
+//                failView.updateFailView("");
+//                break;
+//        }
+//    }
+//
+//
+//    public void updatePage(String logInView, LogInView page) {
+//        switch(logInView) {
+//            case "Passwords don't match.":
+//                LogInView failView = (LogInView) page;
+//
+//                // call Interactor & Presenter
+//                // sending back to ui
+//
+//                // don't have a  view message yet
+//                failView.updateFailView("");
+//                break;
+//        }
+//    }
+//
+//
+//}
--- src/main/java/UseCases/user_register_use_case/UserRegInteractor.java
+++ src/main/java/UseCases/user_register_use_case/UserRegInteractor.java
@@ -1,34 +1,34 @@
-package UseCases.user_register_use_case;
-import Entities.User;
-import Entities.UserDataClasses.UserFactory;
-import InterfaceAdapters.UserRegPresenter;
-//this class normally will implement UserRegInputBoundary interface, it is currently underconstruction tho.
-public class UserRegInteractor {
-//    final UserRegGateway userRegGateway;
-//    final UserFactory userFactory;
-//    final UserRegPresenter presenter;
-//
-//    public UserRegInteractor(UserRegGateway userRegGateway, UserFactory userFactory, UserRegPresenter presenter) {
-//        this.userRegGateway = userRegGateway;
-//        this.userFactory = userFactory;
-//        this.presenter = presenter;
-//    }
-//
-//    @Override
-//    public UserRegResponseModel create(UserRegRequestModel requestModel) {
-//        if (userRegGateway.existsByName(UserRegRequestModel.getName().toString())) {
-//            return InterfaceAdapters.UserRegPresenter.prepareFailView("Username already exists.");
-//        }
-//        else if (!requestModel.getPassword().equals(requestModel.getRepeatPassword())) {
-//            return InterfaceAdapters.UserRegPresenter.prepareFailView("Passwords don't match.");
-//        }
-//
-//        User user = userFactory.createUser(requestModel.getName(), requestModel.getPassword());
-//
-//        UserRegDsRequestModel userDsModel = new UserRegDsRequestModel(requestModel.getName().toString(), requestModel.getPassword());
-//        userRegGateway.save(userDsModel);
-//
-//        UserRegResponseModel accountResponseModel = new UserRegResponseModel(user.getUsername().toString());
-//        return UserRegPresenter.prepareSuccessView(accountResponseModel);
-//    }
-}
+//package UseCases.user_register_use_case;
+//import Entities.User;
+//import Entities.UserDataClasses.UserFactory;
+//import InterfaceAdapters.UserRegPresenter;
+////this class normally will implement UserRegInputBoundary interface, it is currently underconstruction tho.
+//public class UserRegInteractor {
+////    final UserRegGateway userRegGateway;
+////    final UserFactory userFactory;
+////    final UserRegPresenter presenter;
+////
+////    public UserRegInteractor(UserRegGateway userRegGateway, UserFactory userFactory, UserRegPresenter presenter) {
+////        this.userRegGateway = userRegGateway;
+////        this.userFactory = userFactory;
+////        this.presenter = presenter;
+////    }
+////
+////    @Override
+////    public UserRegResponseModel create(UserRegRequestModel requestModel) {
+////        if (userRegGateway.existsByName(UserRegRequestModel.getName().toString())) {
+////            return InterfaceAdapters.UserRegPresenter.prepareFailView("Username already exists.");
+////        }
+////        else if (!requestModel.getPassword().equals(requestModel.getRepeatPassword())) {
+////            return InterfaceAdapters.UserRegPresenter.prepareFailView("Passwords don't match.");
+////        }
+////
+////        User user = userFactory.createUser(requestModel.getName(), requestModel.getPassword());
+////
+////        UserRegDsRequestModel userDsModel = new UserRegDsRequestModel(requestModel.getName().toString(), requestModel.getPassword());
+////        userRegGateway.save(userDsModel);
+////
+////        UserRegResponseModel accountResponseModel = new UserRegResponseModel(user.getUsername().toString());
+////        return UserRegPresenter.prepareSuccessView(accountResponseModel);
+////    }
+//}
