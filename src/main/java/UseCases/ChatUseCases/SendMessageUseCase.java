62f25c9496c06c999616fe7b3235590fd2c66229
Rudy <rudraksh.monga@mail.utoronto.ca>
2022-11-30 15:23:08 -0500

    Added some more Javadoc comments.

--- src/main/java/UseCases/ChatUseCases/SendMessageUseCase.java
+++ src/main/java/UseCases/ChatUseCases/SendMessageUseCase.java
@@ -5,12 +5,29 @@ import Entities.Message;
 import Entities.MessageFactory;
 import Entities.User;
 
+/**
+ * SendMessageUseCase is a Use Case class that is responsible for
+ * sending messages in chats. It is typically instantiated when a
+ * User sends a message to another Use in their Chatroom.
+ *
+ * @author Rudy
+ * @since 1.0
+ */
 public class SendMessageUseCase {
 
     // arguments:
     // the current chatroom (chat)
     // the user who sent the message (user)
     // the message sent (message)
+
+    /**
+     * Adds a Message object into the List within the provided
+     * Chatroom to keep track of the conversation.
+     *
+     * @param chat  Chatroom object where conversation is taking place.
+     * @param user  User object representing the user that sent the message.
+     * @param message   The content of the message sent.
+     */
     public void addMessage(Chatroom chat, User user, String message) {
         MessageFactory fac = new MessageFactory();
         Message new_message = fac.create(user, message);
