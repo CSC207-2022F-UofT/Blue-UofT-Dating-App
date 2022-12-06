707b339eb5344b94a2d1b53b4eac9bb76305158c
vvickychan <wantongvchan@gmail.com>
2022-12-06 00:23:06 -0500

    Javadocs for UserReg use case and the UserRegGateway

--- src/main/java/UseCases/DataRetrieval/UserRegGateway.java
+++ src/main/java/UseCases/DataRetrieval/UserRegGateway.java
@@ -3,18 +3,21 @@ package UseCases.DataRetrieval;
 import Entities.User;
 import Entities.UserGraph;
 
+/**
+ * This class will access the most recent graph and compare to a user
+ */
 public class UserRegGateway {
     /**
-     * this method will return true if the username exists
-     * Otherwise, false will indicate that the username is valid to use
-     * @param  newName          a username that a new user wants
-     * @throws RuntimeException throws runtime exception
-     * @return                  whether a user with the newName exists
-     * @see    User
-     * @see    UserGraph
+     * This method will return true if the username exists otherwise,
+     * false will indicate that the username is valid to use
+     * @param newName the new username to check
+     * @return whether a user with the newName exists
+     * @see User
+     * @see UserGraph
+     * @see CurrentGraph
      */
 
     public boolean getUserExists(String newName) {
-        return (CurrentGraph.getGraph().getUserByString(newName) != null);
+        return CurrentGraph.getGraph().getUserByString(newName) != null;
     }
 }
